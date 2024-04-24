474ac539d07a6c4cffa6256c5a5173e738d3ff96
Khile <khilek@gmail.com>
2024-04-24 15:14:18 -0600

    Backend completed for Tower Events

--- server/src/controllers/TowerEventsController.js
+++ server/src/controllers/TowerEventsController.js
@@ -5,46 +5,74 @@ import BaseController from "../utils/BaseController.js";
 
 
 
-export class TowerEventsController extends BaseController{
-constructor(){
-  super('api/events')
-  this.router
-    .get('', this.getEvents)
-    .get('/:eventId', this.getEventById)
-    .use(Auth0Provider.getAuthorizedUserInfo)
-    .post('', this.createEvents)
+export class TowerEventsController extends BaseController {
+  constructor() {
+    super('api/events')
+    this.router
+      .get('', this.getEvents)
+      .get('/:eventId', this.getEventById)
+      .use(Auth0Provider.getAuthorizedUserInfo)
+      .post('', this.createEvents)
+      .put('/:eventId', this.editEventsById)
+      .delete('/:eventId', this.closeEvent)
 
-}
- async getEvents(request, response, next) {
+  }
+  async getEvents(request, response, next) {
     try {
-     const events = await towerEventsService.getEvents()
-     response.send(events)
+      const events = await towerEventsService.getEvents()
+      response.send(events)
     } catch (error) {
       next(error)
     }
   }
 
-async createEvents(request, response, next) {
-  try {
-    const user = request.userInfo
-    const eventData = request.body
-    eventData.creatorId = user.id
- const event = await towerEventsService.createEvents(eventData)
- response.send(event)
-    
-  } catch (error) {
-    next(error)
-  }
-}
+  async createEvents(request, response, next) {
+    try {
+      const user = request.userInfo
+      const eventData = request.body
+      eventData.creatorId = user.id
+      const event = await towerEventsService.createEvents(eventData)
+      response.send(event)
 
-async getEventById(request, response, next) {
-  try {
-    const eventId = request.params.eventId
-    const events = await towerEventsService.getEventById(eventId)
-    response.send(events)
-  } catch (error) {
-    next(error)
+    } catch (error) {
+      next(error)
+    }
   }
-}
+
+  async getEventById(request, response, next) {
+    try {
+      const eventId = request.params.eventId
+      const events = await towerEventsService.getEventById(eventId)
+      response.send(events)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+
+  async editEventsById(request, response, next) {
+    try {
+      const eventId = request.params.eventId
+      const eventData = request.body
+      const events = await towerEventsService.editEventsById(eventId, eventData)
+      response.send(events)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async closeEvent(request, response, next) {
+    try {
+      const eventId = request.params.eventId
+      const userId = request.userInfo.id
+      const message = await towerEventsService.closeEvent(eventId, userId)
+      response.send(message)
+    } catch (error) {
+      next(error)
+    }
+
+  }
+
+
 
 }
--- server/src/services/TowerEventsService.js
+++ server/src/services/TowerEventsService.js
@@ -1,27 +1,52 @@
 import { dbContext } from "../db/DbContext.js"
+import { Forbidden } from "../utils/Errors.js"
 
 
 
 
-class TowerEventsService{
+class TowerEventsService {
 
- async getEvents() {
-  const events = await dbContext.Events.find()
-  return events
+  async getEvents() {
+    const events = await dbContext.Events.find()
+    return events
   }
 
- async createEvents(eventData) {
+  async createEvents(eventData) {
     const events = await dbContext.Events.create(eventData)
     await events.populate('creator')
     return events
   }
 
- async getEventById(eventId) {
+  async getEventById(eventId) {
     const events = await dbContext.Events.findById(eventId)
-    if(!events) throw new Error(`No Event with that ID: ${eventId}`)
+    if (!events) throw new Error(`No Event with that ID: ${eventId}`)
     return events
   }
-  
+
+  async editEventsById(eventId, eventData) {
+    const eventToUpdate = await this.getEventById(eventId)
+
+    eventToUpdate.name = eventData.name ?? eventToUpdate.name
+    eventToUpdate.description = eventData.description ?? eventToUpdate.description
+    eventToUpdate.coverImg = eventData.coverImg ?? eventToUpdate.coverImg
+    eventToUpdate.location = eventData.location ?? eventToUpdate.location
+    eventToUpdate.capacity = eventData.capacity ?? eventToUpdate.capacity
+    eventToUpdate.startDate = eventData.startDate ?? eventToUpdate.startDate
+
+    await eventToUpdate.save()
+
+    return eventToUpdate
+  }
+
+  async closeEvent(eventId, userId) {
+    const eventToClose = await this.getEventById(eventId)
+    if (eventToClose.creatorId != userId) throw new Forbidden('You cannot close what you did not create')
+    eventToClose.isCanceled = !eventToClose.isCanceled
+    await eventToClose.save()
+
+    return `${eventToClose.name} has been closed!`
+  }
+
 
 }
 
